3c206fc7fb96d60440afd59b5b8749300d6a2ec9
sunnyvk <115800794+sunnyvk@users.noreply.github.com>
2023-02-23 13:21:14 +0530

    Add files via upload

---
+++ Master/CenterSetup/packageMaster.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose')
+
+const packageMasterSchema = mongoose.Schema(
+    {
+        Sr_No: {
+            type: String,
+            required: true,
+        },
+        Service: {
+            type: String,
+            required: true,
+        },
+        Variation: {
+            type: String,
+            required: true,
+        },
+        Package_Name: {
+            type: String,
+            required: true,
+        },
+        Duration: {
+            type: String,
+            required: true
+        },
+        Fees: {
+            type: String,
+            required: true
+        },
+        Status: {
+            type: Boolean,
+            required: true
+        },
+        Action: {
+            type: String,
+            required: true
+        },
+        
+    },
+)
+
+const PackageMaster = mongoose.model('PackageMaster', packageMasterSchema);
+
+
+module.exports = PackageMaster;
