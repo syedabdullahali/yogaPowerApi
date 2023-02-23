24386d1401d108c22a7c8f03416d579d163b790c
sunnyvk <115800794+sunnyvk@users.noreply.github.com>
2023-02-23 13:21:47 +0530

    Add files via upload

---
+++ Navbar/appointment.js
@@ -0,0 +1,60 @@
+const mongoose = require('mongoose')
+
+const appointmentSchema = mongoose.Schema(
+    {
+        Sr_No: {
+            type: String,
+            required: true,
+        },
+        Booking_Date: {
+            type: String,
+            required: true,
+        },
+        Client_Name: {
+            type: String,
+            required: true,
+        },
+        Client_Number: {
+            type: String,
+            required: true,
+        },
+        Appointment_Type: {
+            type: String,
+            required: true
+        },
+        Appointment_With: {
+            type: String,
+            required: true
+        },
+        Appointment_Date: {
+            type: String,
+            required: true
+        },
+        Appointment_Time: {
+            type: String,
+            required: true
+        },
+        Fees_Status: {
+            type: String,
+            required: true
+        },
+        Amount: {
+            type: String,
+            required: true
+        },
+        Status: {
+            type: Boolean,
+            required: true
+        },
+        Staff: {
+            type: String,
+            required: true
+        },
+        
+    },
+)
+
+const Appointment = mongoose.model('Appointment', appointmentSchema);
+
+
+module.exports = Appointment;
