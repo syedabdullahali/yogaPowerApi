376710d6d6b3af343b2663f1f8d1481c58fe7615
sunnyvk <khambayatsunny278@gmail.com>
2023-02-27 13:31:11 +0530

    first

--- Employee/EmployeeTarget/salesTarget.js
+++ Employee/EmployeeTarget/salesTarget.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 const salesTargetSchema = mongoose.Schema(
     {
         Sr_No:{
-            type: Number,
+            type: String,
             required:true
         },
         Employee:{
