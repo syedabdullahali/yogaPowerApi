0e88a35e8fc56614b98e4ae2a1d4d88c2ebc4d8f
syedabdullahali <syedabdullah380@gmail.com>
2023-04-25 20:33:25 +0530

    new route

--- Models/memberCallReport.js
+++ Models/memberCallReport.js
@@ -1,9 +1,9 @@
-// let mongoose = require('mongoose')
-// let callReport = new mongoose.Schema({
-//     username:String,
-//     callTiming:String,
-// }, { timestamps: true })
+let mongoose = require('mongoose')
+let callReport = new mongoose.Schema({
+    username:String,
+    callTiming:String,
+}, { timestamps: true })
 
-// const MemBerCallReport  = mongoose.model('MemBerCallReport', callReport);
+const MemBerCallReport  = mongoose.model('MemBerCallReport', callReport);
 
-// module.exports =  MemBerCallReport
+module.exports =  MemBerCallReport
