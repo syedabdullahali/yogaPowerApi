5a625e44792369596485fb1a5d0b840ba5634919
syedabdullahali <syedabdullah380@gmail.com>
2023-04-25 20:46:47 +0530

    added new

--- Routes/memberCallReport.js
+++ Routes/memberCallReport.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-//modelName
+
 const memberCallReport = require('../Models/memBerCallReport')
 
 router.get('/all', async function (req, res) {
