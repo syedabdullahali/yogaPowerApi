2d378532a1d5e0ba5de06d6eb29fe7e78952e4c1
syed abdullah ali <87133797+syedabdullahali@users.noreply.github.com>
2023-04-27 16:21:40 +0530

    Update clothes.js

--- Inventory/StockListing/clothes.js
+++ Inventory/StockListing/clothes.js
@@ -4,51 +4,39 @@ const ClothesSchema = mongoose.Schema(
     {
         Sr_No: {
             type: String,
-            required: true,
         },
         Product_Code: {
             type: String,
-            required: true,
         },
         Product_Name: {
             type: String,
-            required: true
         },
         Brand_Name: {
             type: String,
-            required: true
         },
         Category: {
             type: String,
-            required: true
         },
         Color: {
             type: String,
-            required: true
         },
         Price: {
             type: String,
-            required: true
         },
         Total_Stock: {
             type: String,
-            required: true
         },
         Sold: {
-            type: String,
-            required: true
+            type: String
         },
         Available_Stock: {
-            type: String,
-            required: true
+            type: String
         },
         Sold_By: {
             type: String,
-            required: true
         },
         Action: {
             type: String,
-            required: true
         },
     },
 )
