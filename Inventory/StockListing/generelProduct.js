242784294d1824dc7101415413d9b71080f4007e
syed abdullah ali <87133797+syedabdullahali@users.noreply.github.com>
2023-05-03 12:54:14 +0530

    Update generelProduct.js

--- Inventory/StockListing/generelProduct.js
+++ Inventory/StockListing/generelProduct.js
@@ -2,9 +2,6 @@ const mongoose = require('mongoose')
 
 const GenerelProductSchema = mongoose.Schema(
     {
-        Sr_No: {
-            type: String,
-        },
         Product_Code: {
             type: String,
         },
@@ -14,29 +11,23 @@ const GenerelProductSchema = mongoose.Schema(
         Brand_Name: {
             type: String,
         },
-        Quantity: {
-            type: String,
-        },
         Price: {
             type: String,
         },
         Total_Stock: {
             type: String,
         },
-        Use: {
-            type: String,
-        },
         Available_Stock: {
-            type: String,
-        },
-        AllotBy: {
-            type: String,
-        },
-        AllotTo: {
             type: String,
         },
         username:{
            type: String,
+        },
+        Category:{
+           type: String,
+        },
+        Color:{
+           type:String,
         }
     },
 )
