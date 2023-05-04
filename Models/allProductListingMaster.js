6b3639d8a1b4876652035ba9360dbe80c8e42259
syed abdullah ali <87133797+syedabdullahali@users.noreply.github.com>
2023-05-04 11:50:02 +0530

    Update allProductListingMaster.js

--- Models/allProductListingMaster.js
+++ Models/allProductListingMaster.js
@@ -1,14 +1,14 @@
 let mongoose = require('mongoose')
 
 let allProductListingMaster = new mongoose.Schema({
-    sataus:Boolean,
+    username: String,
+    sataus: String,
     productCategory:String,
     productName:String,
     brandName:String,
     category:String,
     productPrize:Stirng,
-    ordersQty:String,    
+    ordersQty:String,  
 }, { timestamps: true })
-
 
 module.exports =   mongoose.model('allProductListingMaster', allProductListingMaster);
