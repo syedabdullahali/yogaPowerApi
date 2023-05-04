6a41141598e6018c76c754d9d4259a8f8c5af198
syed abdullah ali <87133797+syedabdullahali@users.noreply.github.com>
2023-05-04 17:16:59 +0530

    Update stockOrderList.js

--- Inventory/StockOrderList/stockOrderList.js
+++ Inventory/StockOrderList/stockOrderList.js
@@ -2,47 +2,36 @@ const mongoose = require('mongoose')
 
 const StockOrderListSchema = mongoose.Schema(
     {
-        Sr_No: {
-            type: String,
-            required: true,
-        },
         Order_Date: {
-            type: String,
-            required: true,
+            type:Date,
         },
         Product_Category: {
             type: String,
-            required: true,
         },
         Product_Name: {
             type: String,
-            required: true
         },
         Brand_Name: {
             type: String,
-            required: true
         },
         Category: {
             type: String,
-            required: true
         },
         Product_Price: {
             type: String,
-            required: true
         },
         Orders_Quantity: {
             type: String,
-            required: true
         },
         Total_Price: {
             type: String,
-            required: true
         },
         Action: {
             type: String,
-            required: true
         },
-        
+        EmployeeName:{
+            type: String,
+        }
     },
 )
 
