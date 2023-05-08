b6e41449b1e8de75a1976d302156163a48dcfea9
syedabdullahali <syedabdullah380@gmail.com>
2023-05-08 16:36:41 +0530

    added new

--- Routes2/StockOrder/StockOrderListReport.js
+++ Routes2/StockOrder/StockOrderListReport.js
@@ -82,7 +82,7 @@ router.get('/:Product_Category',async(req,res)=>{
 router.get('/alert', async(req, res) => {
     try{
         const receivedStockList = await StockOrderList.find({StatOfStock:'InStock'})
-        const alertData = resivedStockListFun(receivedStockList).filter((el)=>(+el))
+        const alertData = resivedStockListFun(receivedStockList)
         res.status(200).json(alertData)
     }
     catch(err){
