0cbde6577d9ddb7a7815cb76af3b3e9d2d3b2bd0
syedabdullahali <syedabdullah380@gmail.com>
2023-05-09 16:33:26 +0530

    added new

--- Models/CheckIns.js
+++ Models/CheckIns.js
@@ -1,19 +1,32 @@
 let mongoose = require('mongoose')
 
-let checkIns = new mongoose.Schema({
+let checkIns  = new mongoose.Schema({
     username: String,
-    centercode: String,
-    date: Date,
-    attendanceId:String,
-    name: String,
-    designation: String,
-    shiftTimeing: Number,
-    startTime: String,
-    endTime: String,
-    checkInTime:String,
-    checkOutTime: String,
-    totalWorkinghour: String,
-    status: String,
+    sataus: String,
+    productCategory:String,
+    productName:String,
+    brandName:String,
+    category:String,
+    productPrize:String,
+    ordersQty:String,
+    Kg:String,
+    Color:String
 }, { timestamps: true })
 
-module.exports = mongoose.model('checkIns', checkIns);
+
+module.exports =   mongoose.model('checkIns ', checkIns );
+
+
+// username: String,
+// centercode: String,
+// date: Date,
+// attendanceId:String,
+// name: String,
+// designation: String,
+// shiftTimeing: Number,
+// startTime: String,
+// endTime: String,
+// checkInTime:String,
+// checkOutTime: String,
+// totalWorkinghour: String,
+// status: String,
